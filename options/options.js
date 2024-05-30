c18064a779b473a1a71efd22302118af723fd760
Crowdbotics <team@crowdbotics.com>
2024-05-30 15:53:30 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "kevin-hofstee-app-48180",
+  url: "https://kevin-hofstee-app-48180.botics.co",
+  api: "https://kevin-hofstee-app-48180.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
